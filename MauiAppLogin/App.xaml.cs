e83f04943623d78a1b7bd23006d998ca8b0705a7
Tiago Casale <casaletiago@gmail.com>
2025-09-06 21:07:37 -0300

    Adicionando arquivos ao projéto.

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,20 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+
+        protected override Window CreateWindow(IActivationState ActivationState)
+        {
+            var window = base.CreateWindow(ActivationState);
+
+            // Formatar a largura e a altura da janela do projeto.
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
+    } // Fecha classe
+
+
+}// Fecha namespace
+
